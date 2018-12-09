3cb1a618c224420f23f2d4d428abfac25b3baf31
SelinaXie <selinaxiem@gmail.com>
2018-12-09 21:06:55 +0800

    Setuptkinter

---
+++ Tkinter/tkinter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 10:46:55 2018
+
+@author: selinaxie
+"""
+
+# =============================================================================
+# 
+# =============================================================================
+from tkinter import *
+
+root = Tk()
+root.geometry("400x400+300+300")
+root.resizable(width=False, height=False) #irresizable
+frame = Frame(root)
+
+root.mainloop()
