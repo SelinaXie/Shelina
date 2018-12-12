c3c3d512234968cb9da3764da3a0b129e730ae8a
SelinaXie <selinaxiem@gmail.com>
2018-12-12 13:39:02 +0800

    RuntimeError

--- chat_system-master/chat_client_class.py
+++ chat_system-master/chat_client_class.py
@@ -11,7 +11,6 @@ import tkinter as Tkinter
 import tkinter.font as tkFont
 
 
-import tk 
 import threading
 
 
@@ -25,6 +24,8 @@ import threading
 # =============================================================================
 
 class Client():
+    title = 'ICS Chat System' 
+    
     def __init__(self, args):
         self.peer = ''
         self.console_input = []
@@ -33,7 +34,54 @@ class Client():
         self.local_msg = ''
         self.peer_msg = ''
         self.args = args
-        self.gui = tk.interface()
+        
+    #interface    
+        self.root = Tkinter.Tk()  
+        self.root.title(self.title)  
+          
+        #窗口面板,用4個面板布局  
+        self.frame = [Tkinter.Frame(),Tkinter.Frame(),Tkinter.Frame(),Tkinter.Frame()]  
+  
+        #顯示消息Text右邊的滾動條  
+        self.chatTextScrollBar = Tkinter.Scrollbar(self.frame[0])  
+        self.chatTextScrollBar.pack(side=Tkinter.RIGHT,fill=Tkinter.Y)  
+          
+        #顯示消息Text，並綁定上面的滾動條  
+        ft = tkFont.Font(family="Fixdsys",size=11)  
+        self.chatText = Tkinter.Listbox(self.frame[0],width=70,height=18,font=ft)  
+        self.chatText['yscrollcommand'] = self.chatTextScrollBar.set  
+        self.chatText.pack(expand=1,fill=Tkinter.BOTH)  
+        self.chatTextScrollBar['command'] = self.chatText.yview()  
+        self.frame[0].pack(expand=1,fill=Tkinter.BOTH)  
+          
+        #標簽，分開消息顯示Text和消息輸入Text  
+        label = Tkinter.Label(self.frame[1],height=2)  
+        label.pack(fill=Tkinter.BOTH)  
+        self.frame[1].pack(expand=1,fill=Tkinter.BOTH)  
+          
+        #輸入消息Text的滾動條  
+        self.inputTextScrollBar = Tkinter.Scrollbar(self.frame[2])  
+        self.inputTextScrollBar.pack(side=Tkinter.RIGHT,fill=Tkinter.Y)  
+          
+        #輸入消息Text，並與滾動條綁定  
+        ft = tkFont.Font(family='Fixdsys',size=11)  
+        self.inputText = Tkinter.Text(self.frame[2],width=70,height=8,font=ft)  
+        self.inputText['yscrollcommand'] = self.inputTextScrollBar.set  
+        self.inputText.pack(expand=1,fill=Tkinter.BOTH)  
+        self.inputTextScrollBar['command'] = self.chatText.yview()  
+        self.frame[2].pack(expand=1,fill=Tkinter.BOTH)  
+          
+        #發送消息按鈕  
+        self.sendButton=Tkinter.Button(self.frame[3],text=' Send ',width=10,command=self.sendM())  
+        self.sendButton.pack(expand=1,side=Tkinter.BOTTOM and Tkinter.RIGHT,padx=15,pady=8)  
+  
+        #關閉按鈕  
+        self.closeButton=Tkinter.Button(self.frame[3],text=' Quit ',width=10,command=self.close)  
+        self.closeButton.pack(expand=1,side=Tkinter.RIGHT,padx=15,pady=8)  
+        self.frame[3].pack(expand=1,fill=Tkinter.BOTH)  
+    
+    def close(self):  
+        sys.exit()
 
     def quit(self):
         self.socket.shutdown(socket.SHUT_RDWR)
@@ -53,6 +101,16 @@ class Client():
 
     def shutdown_chat(self):
         return
+    
+    def sendM(self):
+        message = self.inputText.get('1.0',Tkinter.END).encode() 
+        try:
+            self.send(message)
+            self.chatText.insert(Tkinter.END,'  ' + message.decode() + '\n')   
+            self.inputText.delete(0.0,message.__len__()-1.0) 
+        except:
+            self.chatText.insert(Tkinter.END,'  ' + message.decode() + '\n')   
+            self.inputText.delete(0.0,message.__len__()-1.0) 
 
     def send(self, msg):
         mysend(self.socket, msg)
@@ -73,7 +131,7 @@ class Client():
 
     def output(self):
         if len(self.system_msg) > 0:
-            self.gui.chatText.insert(Tkinter.END,'  ' + self.system_msg + '\n') 
+            self.chatText.insert(Tkinter.END,'  ' + self.system_msg + '\n') 
             
             self.system_msg = ''
 
@@ -99,22 +157,20 @@ class Client():
 
     def read_input(self):
         while True:
-            
-#            text = self.gui.inputText.get('1.0',Tkinter.END) 
-            try:
-                text = self.gui.sendMessage() 
+            text = self.inputText.get('1.0',Tkinter.END)
+            if text == '':
+                continue
+            else:
+                
 #            text = sys.stdin.readline()[:-1]
-                self.gui.chatText.insert(Tkinter.END,'  a' + text + '\n')
+                self.chatText.insert(Tkinter.END,'  a' + text + '\n')
                 self.console_input.append(text) # no need for lock, append is thread safe
                 continue
-            except:
-                self.gui.root.update()
                 
     def print_instructions(self):
         self.system_msg += menu
 
     def run_chat(self):
-        self.gui.run()
         self.init_chat()
         self.system_msg += 'Welcome to ICS chat\n'
         self.system_msg += 'Please enter your name: '
